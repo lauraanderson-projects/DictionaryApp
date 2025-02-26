678e551d5cb2cb412b1edfc6328535a0990cf11f
sphirebluela <laura.sphireblue@gmail.com>
2025-02-26 15:04:42 -0800

    api documentation

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,6 +11,7 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
+    //https://www.shecodes.io/learn/apis/dictionary
     const apiKey = "bd809658a5b50o74b7f3fe9fa5dft8a8";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     //console.log(apiUrl);
