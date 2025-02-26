5b380fc54787cead4a89f6bd091c0a0c5e2ec07c
sphirebluela <laura.sphireblue@gmail.com>
2025-02-26 15:03:53 -0800

    added axios and cleanup

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(keyword);
+    const apiKey = "bd809658a5b50o74b7f3fe9fa5dft8a8";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    //console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
