02264658ecc025671b154d8d0e3071a2bd9a0ab2
sphirebluela <laura.sphireblue@gmail.com>
2025-02-26 21:20:03 -0800

    turn off logs

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,10 +6,14 @@ import Results from "./Results";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
   let [results, setResults] = useState(null);
+  const [hasError, setHasError] = useState(false);
 
   function handleResponse(response) {
-    //console.log(response.data.meanings[0].definition);
     setResults(response.data);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
   }
 
   function search(event) {
@@ -17,12 +21,7 @@ export default function Dictionary() {
     //https://www.shecodes.io/learn/apis/dictionary
     const apiKey = "bd809658a5b50o74b7f3fe9fa5dft8a8";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    //console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
-  }
-
-  function handleKeywordChange(event) {
-    setKeyword(event.target.value);
   }
 
   return (
